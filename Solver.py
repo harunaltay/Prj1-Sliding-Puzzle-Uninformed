e7155ce48fc0bf94dc6f88e16aebfa47c2f04a77
Harun Altay <h.altay@gmail.com>
2019-03-05 22:31:19 +0300

    report_to_string() bitti ... DFS 'ye geçiyorum

--- Solver.py
+++ Solver.py
@@ -19,8 +19,47 @@ class Solver:
         self.solution_matrix = create_a_matrix_solution(self.n)
         self.solution_vector = create_a_vector_from_a_matrix(self.solution_matrix)
 
+        self.time_start = 0
+        self.time_stop = 0
+        self.time_elapsed = 0
+
+        self.message_short = ""
+        self.message_long = ""
+
+        self.answer_list = []
+
+        self.counter_loop = 0  # as well, number_of_nodes_expanded, extracted from the fringe
+        self.counter_number_of_nodes_appended = 0
+
     def solve(self):
-        s = "solving ... in Solver ..."
+        # empty solution.. this class is not ment to be used. Use, for example, SolverBFS.
+        return self.answer_list, self.message_short, self.message_long, self.time_elapsed
+
+    def report_to_string(self):
+        s = ""
+
+        s += "**************************************************\n"
+        s += "*** Solution found!\n"
+        s += "The number of moves/steps to the solution: " + str(len(self.answer_list) - 1) + " moves.\n"
+        s += "time_elapsed: " + str(self.time_elapsed) + "\n"
+        s += "message_short: " + str(self.message_short) + "\n"
+        s += "message_long: " + self.message_long + "\n"
+        s += "counter_loop (number of expanded/extracted from the fringe nodes): " + str(self.counter_loop) + "\n"
+        s += "counter_number_of_nodes_appended: " + str(self.counter_number_of_nodes_appended) + "\n"
+        s += "\n"
+        s += "Starting from the given/initial puzzle, ends with the goal.\n"
+        s += "\n"
+
+        step = 0
+        for matrix in self.answer_list:
+            s += "step: " + str(step) + "\n\n"
+            for line in matrix:
+                s += str(line) + "\n"
+            s += "\n"
+            step += 1
+
+        s += "***************************************************\n"
+
         return s
 
 
--- SolverBFS.py
+++ SolverBFS.py
@@ -12,32 +12,43 @@ class SolverBFS (Solver):
         super().__init__(matrix)
         self.queue = deque()
 
+    # Solves the puzzle
     def solve(self):
-        print("solving begins ... in Solver_BFS ...")
+        self.time_start = time.time()
+
+        if not self.is_solvable:
+            print("The puzzle is not a solvable puzzle. Check it before calling this method. halting.")
+            raise Exception('The puzzle is not a solvable puzzle. Check it before calling this method. halting.')
+
+        # print("solving begins ... in Solver_BFS ...")
 
         self.queue.append(self.node_initial)
 
-        counter_loop = 0  # number_of_nodes_expanded, extracted from the fringe
-        counter_number_of_nodes_appended = 0
-
+        # turns until finds one of the optimal solutions
+        # assumes that the puzzle has a solution.. see: "solver.is_solvable" attribute of Solver
         while True:
-            counter_loop += 1
-            node_popped = self.queue.popleft()
+            self.counter_loop += 1
 
             # print("popped from the fringe:", node_popped.board.matrix)
-            # print("aşağıdaki matrisler fringe'ye append ediliyor ...")
+            node_popped = self.queue.popleft()
 
+            # pop ettiğimiz node, bir çözüm node'si mi?
             if is_this_matrix_one_of__the_solutions(node_popped.board.matrix):
-                print()
-                print("*** bulundu:", node_popped.board.matrix)
-                print("counter_loop (number of expanded/extracted from the fringe nodes):", counter_loop)
-                print("counter_number_of_nodes_appended:", counter_number_of_nodes_appended)
-                print()
-                answer_list = trace_to_the_initial_node_from_the_solution_node(node_popped)
-                return answer_list
+                # print("*** bulundu:", node_popped.board.matrix)
+                self.answer_list = trace_to_the_initial_node_from_the_solution_node(node_popped)
+                self.time_stop = time.time()
+                self.time_elapsed = self.time_stop - self.time_start
+
+                return self.answer_list, self.message_short, self.message_long, self.time_elapsed
+
+            # pop edilen node 'bir solution' değilmiş...
+            # ... öyleyse bu node'yi patlat, expand et, ...
+            # ... sonra da fringe'ye append et, grand-parent'i ile aynı olan node hariç.
 
             board_list_neighbors = node_popped.board.get_neighbors()
 
+            # generate edilen her bir yeni node'yi fringe'ye append et...
+            # ... tabi, grand-parent'i ile aynı olan birisi hariç
             for board_item in board_list_neighbors:
 
                 # bu child grand_parent ile aynıysa, geç, continue ...
@@ -47,7 +58,7 @@ class SolverBFS (Solver):
                     if is_child_and_grand_parent_same:
                         continue
 
-                counter_number_of_nodes_appended += 1
+                self.counter_number_of_nodes_appended += 1
 
                 # bu child'i append et ...
                 node_new = Node(board_item, node_popped)
@@ -56,9 +67,9 @@ class SolverBFS (Solver):
                 #     print(line)
                 # print()
 
-            # print(counter_loop, "loop ----------------------------------------------------------------------------")
+            # print(counter_loop, "loop ---------------------------------------------------------")
             # garanti olsun diye. yine de 1.000.000 loop gerektirecek puzzle olur mu bilmem. Olursa bakarız.
-            if counter_loop == 1000000:
+            if self.counter_loop == 1000000:
                 print("loop 1.000.000 oldu. Çalışma kesildi.")
                 break
 
@@ -75,35 +86,31 @@ def test_stub_solver_bfs_main():
     # f_name = 'puzzle-text-files/puzzle4x4-01.txt'
     # f_name = 'puzzle-text-files/puzzle4x4-unsolvable.txt'
 
-    vector, matrix = create_a_vector_and_a_matrix_from_a_text_file(f_name)
+    test_stub_solver_bfs_main_from_file_name(f_name)
 
-    print(vector)
+
+def test_stub_solver_bfs_main_from_file_name(file_name):
+    vector, matrix = create_a_vector_and_a_matrix_from_a_text_file(file_name)
+
+    test_stub_solver_bfs_main_from_the_matrix(matrix)
+
+
+def test_stub_solver_bfs_main_from_the_matrix(matrix):
     for line in matrix:
         print(line)
+
+    print()
 
     solver = SolverBFS(matrix)
 
     if not solver.is_solvable:
-        print("this puzzle is not solvable... program terminates...")
+        print("this puzzle is not solvable... method returns...")
         return
 
-    start = time.time()
+    answer_list, message_short, message_long, time_elapsed = solver.solve()
 
-    answer_list = solver.solve()
-
-    end = time.time()
-    print("solved in", end - start, "seconds")
-
-    print("the number of steps to the solution:", len(answer_list) - 1)
-    print()
-    print("****************************************************************************************")
-
-    for matrix in answer_list:
-        for line in matrix:
-            print(line)
-        print()
-
-    print("****************************************************************************************")
+    s = solver.report_to_string()
+    print(s)
 
 
 if __name__ == '__main__':
